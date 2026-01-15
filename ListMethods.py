862e111ed6235c52551df6913215b6b8a477a172
Bhushan Rajvanshi <rajdevram7070@gmail.com>
2026-01-15 13:07:49 +0530

    Basics List in Python

--- ListMethods.py
+++ ListMethods.py
@@ -1,17 +1,37 @@
 
-# Tacking input
-name = input("Enter your name: ")
-work = input("Enter your work: ")
-live = input("Enter your live: ")
-
-print("My name is "+ name + ", I work in " + work + " and I live in " + live)
-
 # ListMethods
-name = ["Bhushan", "Shivam", "Shivansh", "Shivansh", "Shivansh"]
+
+name = ["Bhushan", "Shivam", "Shivansh"]
 print(name)
 print(name[0])
 print(name[1])
 print(name[2])
-print(name[3])
-print(name[4])
 print(type(name))
+
+
+items = ["Apple", "Banana", "Orange", "Watermelon"]
+print(len(items))
+items.append("Mango")
+# items.append(["Pineapple", "Guava"])
+items.extend(["Pineapple", "Guava"])
+items.insert(2, "Kiwi")
+print(items)
+print(items.index("Watermelon"))
+print(items.count("Apple"))
+items.remove("Watermelon")
+print(items)
+items.pop(1)  # Remove by index
+print(items)
+items.clear()  # Clear the list
+print(items)  # Empty list
+
+
+numbers = [1, 6, 2, 8, 3, 9, 4, 88, 22, 55, 22, 77]
+numbers.sort()  # Sort the list
+print(numbers)
+numbers.sort(reverse=True)  # Sort the list in reverse order
+print(numbers)
+numbers.reverse()  # Reverse the list
+print(numbers)
+print(88 in numbers)
+print(99 in numbers)
