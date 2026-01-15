fa56cc2de231fcc67d03af82e1a0d7440ebfcc8a
Bhushan Rajvanshi <rajdevram7070@gmail.com>
2026-01-15 12:43:10 +0530

    List Methods in Python

---
+++ ListMethods.py
@@ -0,0 +1,17 @@
+
+# Tacking input
+name = input("Enter your name: ")
+work = input("Enter your work: ")
+live = input("Enter your live: ")
+
+print("My name is "+ name + ", I work in " + work + " and I live in " + live)
+
+# ListMethods
+name = ["Bhushan", "Shivam", "Shivansh", "Shivansh", "Shivansh"]
+print(name)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(type(name))
