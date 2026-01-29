e989bd630ac0f9d9ce8a09ba25b9345382de30f6
Bhushan Rajvanshi <rajdevram7070@gmail.com>
2026-01-29 10:31:25 +0530

    Conditional in python

---
+++ Condition.py
@@ -0,0 +1,18 @@
+a = 25
+if a>=30 :
+    print("You can get married")
+elif a>=18 :
+    print("Eligible for vote")
+else :
+    print("Not eligible for vote")
+
+
+
+if a%2==0 :
+    print("A is Even numbers")
+else :
+    print("A is Odd numbers")
+
+# Ternary operator
+status = "Adult" if a>=18 else "Minor"
+print(status)
