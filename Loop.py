58f5729669e3afa238776a5b5ad9da1182b7c363
Bhushan Rajvanshi <rajdevram7070@gmail.com>
2026-01-29 10:28:40 +0530

    Loop in python

---
+++ Loop.py
@@ -0,0 +1,46 @@
+items = ["bhushan", "shivam", 46, "shivansh"]
+
+for item in items :
+    print(item)
+
+
+# for i in range(1, 20) :
+#     print(i)
+
+
+# for i in range(1, 50, 5) :   # increment by 5
+    # print(i)
+
+
+# for i in range(50, 1, -5) :   # decrement by 5
+    # print(i)  
+
+# Conditions
+# for i in range(1, 20) :
+    # if i%2==0 :
+        # print(i)
+
+
+#  Continue
+# for i in range(1, 20) :
+    # if i%2==0 :
+        # continue
+    # print(i)
+
+
+#  Break
+for i in range(1, 20) :
+    if i>=10 :
+        break
+    print(i)
+
+
+a = "Bhushan"
+for i in a :
+    print(i)
+
+
+num = 1
+while num<=10 :
+    print(num)
+    num += 1
